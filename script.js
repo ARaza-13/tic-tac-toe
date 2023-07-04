534b1c06accd0f16bddef355cd06afc1dae0b778
Ali Raza <aliraza021419@gmail.com>
2023-07-04 17:41:18 -0400

    Added functions to the Game module that renders the content of the gameboard array to the webpage

--- script.js
+++ script.js
@@ -35,19 +35,50 @@ const Player = (name, symbol) => {
 const Game = (() => {
     const player1 = Player('Player 1', 'X');
     const player2 = Player('Player 2', 'O');
-    let currentPlayer = player1;
+    let currentPlayer;
+    let gameboard;
 
     // instance of Gameboard module
-    const gameboard = Gameboard;
+    const initialize = () => {
+        gameboard = Gameboard;
+        gameboard.initializeBoard();
+        currentPlayer = player1;
+        renderBoard();
+        addCellEventListeners();
+    }
+
+    const renderBoard = () => {
+        const cells = document.querySelectorAll('#game-board td');
+
+        cells.forEach((cell, index) => {
+            cell.textContent = gameboard.getCellValue(index) || '';
+        });
+    }
+
+    const addCellEventListeners = () => {
+        const cells = document.querySelectorAll('#game-board td');
+        
+        cells.forEach((cell, index) => {
+            cell.addEventListener('click', () => {
+                handleCellClick(index);
+            });
+        });
+    }
+    
 
     // update gameboard based on clicked cell index
     const handleCellClick = (index) => {
+        console.log(index);
+        console.log(gameboard.getCellValue(index))
         if (gameboard.getCellValue(index) === null) {
             gameboard.updateCell(index, currentPlayer.getSymbol());
+            renderBoard();
         }
     }
 
     return {
-        handleCellClick,
+        initialize,
     }
 })();
+
+Game.initialize();
