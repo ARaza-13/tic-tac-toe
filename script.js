da12b085e083f699e2ed7c682da32e4b5677b872
Ali Raza <aliraza021419@gmail.com>
2023-07-04 21:19:25 -0400

    Added functions to Game module to check for all possible winning combinations or if a tie has occured

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ const Gameboard = (() => {
         board = Array(9).fill(null);
     }
 
+    const getBoard = () => {
+        return board;
+    }
+
     const getCellValue = (index) => {
         return board[index];
     }
@@ -13,23 +17,16 @@ const Gameboard = (() => {
         board[index] = symbol;
     }
 
-    // additional methods for checking win/draw conditions
-
     return {
         initializeBoard,
+        getBoard,
         getCellValue,
         updateCell,
     };
 })();
 
 const Player = (name, symbol) => {
-    const getName = () => name;
-    const getSymbol = () => symbol;
-
-    return {
-        getName,
-        getSymbol,
-    };
+    return { name, symbol };
 };
 
 const Game = (() => {
@@ -37,6 +34,36 @@ const Game = (() => {
     const player2 = Player('Player 2', 'O');
     let currentPlayer;
     let gameboard;
+
+    const checkWin = () => {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6] // Diagonals
+        ];
+        for (let combiantion of winningCombinations) {
+            const [a, b, c] = combiantion;
+            const cellA = gameboard.getCellValue(a);
+            const cellB = gameboard.getCellValue(b);
+            const cellC = gameboard.getCellValue(c);
+
+            if (cellA && cellA === cellB && cellA === cellC) {
+                return cellA; // return winning player's symbol
+            }
+        }
+
+        return null; // if no winning combination is found
+    }
+
+    const checkTie = () => {
+        for (let value of gameboard.getBoard()) {
+            if (value === null) {
+                return false; // Game is not a tie yet
+            }
+        }
+
+        return true; // Game is a tie
+    }
 
     // instance of Gameboard module
     const initialize = () => {
@@ -72,9 +99,20 @@ const Game = (() => {
     // update gameboard based on clicked cell index
     const handleCellClick = (index) => {
         if (gameboard.getCellValue(index) === null) {
-            gameboard.updateCell(index, currentPlayer.getSymbol());
+            gameboard.updateCell(index, currentPlayer.symbol);
             renderBoard();
-            switchPlayer();
+
+            if (checkWin()) {
+                console.log(`${currentPlayer.symbol} wins!`)
+                // game is won
+                // add logic to handle game over, display winner, etc.
+            } else if (checkTie()) {
+                console.log("It's a tie")
+                // game is tie
+                // add logic to handle game over, display tie, etc.
+            } else {
+                switchPlayer();
+            }
         }
     }
 
